5f4bee0a9e2f0db4790b9a82848691858f8d651f
Er Lor3xxone <128195197+Loreehh@users.noreply.github.com>
2023-05-11 18:56:34 +0200

    javascript initialed

---
+++ HTMLxCSSxJS/index.js
@@ -0,0 +1,10 @@
+HTMLElementObject.innerHTML=text
+
+<p id="power_of_javascript_demo">The power of Javascript.</p>
+
+
+<script>
+function myFunction() { 
+    document.getElementById("power_of_javascript_demo").innerHTML = "JavaScript, you're powerful!";
+}
+</script>
